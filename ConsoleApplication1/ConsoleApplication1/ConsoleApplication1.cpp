3a2bafd8d51817b62fd8aa3f1f532f072471dd6c
akio1230 <huangxinqiuwei@gmail.com>
2026-06-02 13:01:34 +0900

    両替プログラム

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int main()
+{
+    int yen;
+
+    printf("1以上の整数を入力してください\n");
+    scanf_s("%d", yen);
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1/coin.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int fh;
+int hun;
+int fifty;
+int tenn;
+int five;
+int one;
+
+int exchange(int value)
+{
+    fh = value / 500;
+	value = value % 500;
+	hun = value / 100;
+	value = value % 100;
+	fifty = value / 50;
+	value = value % 50;
+	tenn = value / 10;
+	value = value % 10;
+	five = value / 5;
+	value = value % 5;
+    one = value / 1;
+	value = value % 1;
+}
+
+void exresult()
+{
+	if (0 != fh)
+	{
+		printf("500‰~‹Ê‚Í %d–‡ ‚Å‚·", fh);
+	}
+	if (0 != hun)
+	{
+		printf("100‰~‹Ê‚Í %d–‡ ‚Å‚·", hun);
+	}
+}
